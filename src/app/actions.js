34ada82ff741798b1a48d3d58b65f4a2ce5ace33
lanwakacsus <lwakamatsu@csus.edu>
2026-03-07 23:46:40 -0800

    Changed AI model to current model being offered on free tier

--- src/app/actions.js
+++ src/app/actions.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY);
 
 export async function askGemini(userMessage) {
-  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
   
   const result = await model.generateContent(userMessage);
   const response = await result.response;
